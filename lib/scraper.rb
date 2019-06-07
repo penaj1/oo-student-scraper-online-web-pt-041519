65337e60a4ed7a1daa606d504fcad22dce293b9f
Janice Pena <jipena94@gmail.com>
2019-06-07 03:02:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,8 @@ class Scraper
         students_hash[:linkedin] = social_link['href']
       elsif social_links["href"].include? "github"
         students_hash[:github] = social_link['href']
-      else 
+      else social_links["href"].include? "github"
+        students_hash[:github] = social_link['href']
     
   end
 
