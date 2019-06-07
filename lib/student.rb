2574723e69fb63bde77585d1005161bafad10591
Janice Pena <jipena94@gmail.com>
2019-06-07 03:13:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,14 +10,13 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
   end
 
   def add_student_attributes(attributes_hash)
-
   end
 
   def self.all
     @@all
   end
+  
 end
