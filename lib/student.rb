21bd99fe2b2d9a4c9f3ac3640d13752bacb104ae
Janice Pena <jipena94@gmail.com>
2019-06-07 03:26:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes.each do |key, value|
-      self.send"#{key}=", value )
+      self.send("#{key}=", value )
       end
     @@all << self
   end
